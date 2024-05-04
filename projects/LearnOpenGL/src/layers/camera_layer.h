ff3d27acaa8504f8390825bf020c6568efc5d685
babaliaris <babaliaris.nikos@gmail.com>
2024-05-04 21:40:10 +0300

    Prepared for the camera tutorial

---
+++ projects/LearnOpenGL/src/layers/camera_layer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../core/learnOpenGL.h"
+
+
+namespace LearnOpenGL
+{
+    class CameraLayer : public LearnOpenGL::Layer
+    {
+        public:
+        CameraLayer();
+        virtual ~CameraLayer();
+
+        virtual void onStart(Application *app) override;
+        virtual void onUpdate(Application *app) override;
+
+        private:
+        Shader *m_shader;
+        Texture *m_texture;
+        unsigned int m_vao, m_vbo;
+        glm::vec3 m_cubePositions[10];
+    };
+}
--- projects/LearnOpenGL/src/main.cpp
+++ projects/LearnOpenGL/src/main.cpp
@@ -2,9 +2,11 @@
 
 #include "layers/coordinate_systems.h"
 #include "layers/more_3D.h"
+#include "layers/camera_layer.h"
 
 #define COORDINATE_SYSTEMS "COORDINATE_SYSTEMS"
 #define MORE_3D "MORE_3D"
+#define CAMERA "CAMERA"
 
 
 int main(void)
@@ -12,7 +14,7 @@ int main(void)
     LearnOpenGL::Application *app = new LearnOpenGL::Application("LearnOpenGL", 800, 600);
 
     //Choose example layer to start.
-    std::string loadingLayer = MORE_3D;
+    std::string loadingLayer = CAMERA;
 
     VAMP_INFO( "Loading: %s", loadingLayer.c_str() );
 
@@ -26,6 +28,12 @@ int main(void)
     {
         LearnOpenGL::More3D *more_2d = new LearnOpenGL::More3D();
         app->appendLayer(more_2d);
+    }
+
+    else if (loadingLayer == CAMERA)
+    {
+        LearnOpenGL::CameraLayer *camera_layer = new LearnOpenGL::CameraLayer();
+        app->appendLayer(camera_layer);
     }
 
     app->Run();
